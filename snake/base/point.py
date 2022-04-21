08886453c4a19bfad6dcccee2ed4d3ad0cfe7e57
Chretien Li <chrecci@gmail.com>
2022-04-21 18:38:47 +0100

    Necessary framework for Snake-AI
    Template based off of another library. Will be adding in A-star search for my project. Provides GUI, testing, and other helper functions

---
+++ snake/base/point.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylint: disable=C0103,C0111
+
+"""Definition of class Point."""
+
+from enum import Enum, unique
+
+
+@unique
+class PointType(Enum):
+    """Type of the points on the game map."""
+    EMPTY = 0
+    WALL = 1
+    FOOD = 2
+
+    # point types include different positions. So head facing down is different than head facing right
+    HEAD_L = 100
+    HEAD_U = 101
+    HEAD_R = 102
+    HEAD_D = 103
+
+    # body can create different angle, or straight vertically vs horizontally
+    BODY_LU = 104
+    BODY_UR = 105
+    BODY_RD = 106
+    BODY_DL = 107
+    BODY_HOR = 108
+    BODY_VER = 109
+
+
+class Point:
+    """Point on the game map."""
+    def __init__(self):
+        self._type = PointType.EMPTY
+
+    @property
+    def type(self):
+        return self._type
+
+    @type.setter
+    def type(self, val):
+        self._type = val
